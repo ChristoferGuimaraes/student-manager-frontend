78a2c570cf2186942613e96267a0d2007f5ce0e6
Christofer Guimarães <christoferguima@gmail.com>
2022-09-05 16:00:57 -0300

    improve table

--- index.ts
+++ index.ts
@@ -1,27 +1,67 @@
 const tableContent: HTMLElement | null = document.getElementById('content');
-
+let path: string = "students"
 
 async function init(): Promise<void> {
     await getAll();
+    console.log(element)
 }
 
 async function getAll(): Promise<void> {
-    await fetch("http://localhost:9090/api/students")
+    await fetch(`http://localhost:9090/api/${path}`)
         .then(res => res.json())
         .then((data) => {
             generateTable(data.content);
         })
 }
 
-function generateElement(tag: any, text?: any) {
+function generateElement(tag: any, text?: string) {
     let elementTag = document.createElement(tag);
     if (text) {
         elementTag.innerText = text;
+
+        //Students
+        if (text == 'id') {
+            elementTag.innerText = "ID";
+        }
+
+        if (text == 'firstName') {
+            elementTag.innerText = "First Name";
+        }
+
+        if (text == 'lastName') {
+            elementTag.innerText = "Last Name";
+        }
+
+        if (text == 'age') {
+            elementTag.innerText = "Age";
+        }
+
+        if (text == 'email') {
+            elementTag.innerText = "Email";
+        }
+
+        //Courses
+        if (text == 'name') {
+            elementTag.innerText = "Name";
+        }
+
+        if (text == 'teacherName') {
+            elementTag.innerText = "Teacher Name";
+        }
+
+        if (text == 'classNumber') {
+            elementTag.innerText = "Class Number";
+        }
+
+        if (text == 'startDate') {
+            elementTag.innerText = "Start Date";
+        }
     }
+
     return elementTag;
 };
 
-function appendElement(parent: any, child: any) {
+function appendElement(parent: HTMLTableElement, child: HTMLTableElement) {
     parent.appendChild(child);
     return parent;
 };
@@ -41,14 +81,32 @@ function generateTable(data: string[]): void {
     );
 
     //values
-    const makeRow = (acc: any, row: any) =>
+    const makeRow = (acc: HTMLTableElement, row: any) =>
         appendElement(acc,
             objectKeysParameters.slice(0, 5)
                 .map(objParam => generateElement('td', row[objParam]))
                 .reduce(appendElement, generateElement('tr'))
         );
+
     data.reduce(makeRow, table);
     tableContent?.appendChild(table);
 };
+
+let element = document.getElementById("btn")
+element?.addEventListener("click", changePath);
+
+let title = document.querySelector(".table-title");
+
+function changePath(): void {
+    path === 'students' ? path = 'courses' : path = 'students';
+
+    let titleContent = title?.textContent?.toString();
+    console.log(titleContent)
+
+    if (tableContent !== null) {
+        tableContent.innerHTML = '';
+    }
+    getAll();
+}
 
 init();
