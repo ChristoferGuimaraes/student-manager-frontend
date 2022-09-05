a48197d1689e637525249ddf10ed7352beb3921b
Christofer Guimarães <christoferguima@gmail.com>
2022-09-05 16:00:37 -0300

    added node server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 5152;
+
+app.listen(port, () => {
+  console.log(`Application started on: localhost:${port}`);
+});
+
+app.use(express.static(__dirname));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/students", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+});
